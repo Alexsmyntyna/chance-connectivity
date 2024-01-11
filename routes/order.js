00cec203673070616f7061d8074c8e89ed48c53b
Kirill-Ryzhkov <slusc10a@gmail.com>
2024-01-11 16:44:16 -0500

    Create success payment method

--- routes/order.js
+++ routes/order.js
@@ -4,6 +4,7 @@ const requireAuth = require("../middleware/requireAuth");
 const {
     createOrder,
     getOrders,
+    successPayment,
     completeOrder
 } = require("../controllers/orderController");
 const corsHeader = require("../middleware/corsHeaders");
@@ -11,12 +12,16 @@ const corsHeader = require("../middleware/corsHeaders");
 const router = express.Router();
 
 router.use(corsHeader);
+
+// success payment
+router.post("/payment/success", successPayment);
+// get orders
+router.get("/", getOrders);
+
 router.use(requireAuth);
 
 // create order
 router.post("/create", createOrder);
-// get orders
-router.get("/", getOrders);
 // complete corder
 router.post("/complete/:id", completeOrder);
 
