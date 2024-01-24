d3f2dae48acff20dabc0567b0dc3461a0d3e1c72
Kirill-Ryzhkov <slusc10a@gmail.com>
2024-01-23 22:39:20 -0500

    Create transfer users functional

--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-
 const Schema = mongoose.Schema;
+
+const generateRandomString = (length = 6) => {
+    return Math.random().toString(20).substr(2, length);
+}
 
 const userSchema = new Schema({
     first_name: {
@@ -39,11 +42,18 @@ const userSchema = new Schema({
     email: {
         type: String,
         required: true,
+    },
+    login: {
+        type: String,
+        required: true,
         unique: true
     },
     password: {
         type: String,
         required: true
+    },
+    password_temp: {
+        type: String
     },
     nfc_id: {
         type: String,
@@ -55,6 +65,9 @@ const userSchema = new Schema({
     },
     event_ids: {
         type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }]
+    },
+    chat_id: {
+        type: String
     }
 });
 
@@ -75,7 +88,7 @@ userSchema.statics.signup = async function (first_name, last_name, role, city, a
         throw Error("Sex is not correct");
     }
 
-    const exists = await this.findOne({ email });
+    const exists = await this.findOne({ login: email });
 
     if (exists) {
         throw Error("Email already in use");
@@ -93,21 +106,19 @@ userSchema.statics.signup = async function (first_name, last_name, role, city, a
         city,
         age,
         sex,
-        email, 
+        email,
+        login: email,
         password: hash,
         balance: 0
     });
 }
 
-userSchema.statics.login = async function(email, password) {
-    if (!email || !password) {
+userSchema.statics.login = async function(login, password) {
+    if (!login || !password) {
         throw Error("All fields must be filled");
     }
-    if (!validator.isEmail(email)) {
-        throw Error("Email is not valid");
-    }
 
-    const user = await this.findOne({ email });
+    const user = await this.findOne({ login });
 
     if (!user) {
         throw Error("Incorrect email");
@@ -125,6 +136,37 @@ userSchema.statics.login = async function(email, password) {
 userSchema.statics.updateBalance = async function() {
     const userList = await this.updateMany({ balance: { $gte: 0 } }, { balance: 1000 });
     return userList;
+}
+
+userSchema.statics.transferUsers = async function(users) {
+    const result = await Promise.all(users.map(async (user) => {
+        const parts = user.email.split('@');
+        const login = parts[0] + "_" + generateRandomString(4);
+
+        const generatePassword = generateRandomString(8);
+        
+        const password = await Bun.password.hash(generatePassword, {
+            algorithm: "bcrypt",
+            cost: 10
+        });
+
+        return {
+            first_name: user.first_name,
+            last_name: user.last_name,
+            role: "participant",
+            city: "System",
+            age: 18,
+            sex: user.gender.toLowerCase(),
+            balance: 0,
+            email: user.email,
+            login,
+            password,
+            password_temp: generatePassword,
+            chat_id: user.chat_id
+        };
+    }));
+    const transfer = await this.insertMany(result);
+    return transfer;
 }
 
 module.exports = mongoose.model("User", userSchema);
